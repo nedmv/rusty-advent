5302a970a8e70485c588a1ffd481cfe1b2e594ad
nedmv <nedmvl@rambler.ru>
2024-12-22 20:22:30 +0300

    fix(2024, day 22): use flat arrays

--- src/day22.rs
+++ src/day22.rs
@@ -1,18 +1,17 @@
 const MOD: usize = 16777216;
 const MAX_ITERATIONS: usize = 2000;
 const MAX_PATTERNS: usize = 19*19*19*19;
-const 
 
+#[inline(always)]
 fn calc(mut num: usize) -> usize {
-  num ^= num * 64;  
+  num ^= num << 6;
   num %= MOD;
-  num ^= num / 32;
+  num ^= num >> 5;
   num %= MOD;
-  num ^= num * 2048;
+  num ^= num << 11;
   num %= MOD;
   num
 }
-
 
 #[aoc(day22, part1)]
 pub fn part1(input: &str) -> usize {
@@ -31,52 +30,30 @@ pub fn part2(input: &str) -> usize {
   let mut cur_cost;
   let mut prev_cost;
   let mut pattern = [0; 4];
-  let mut data = [[[[0; 19]; 19]; 19]; 19];
-  let mut seen = [[[[false; 19]; 19]; 19]; 19];
+  let mut id;
+  let mut data = [0; MAX_PATTERNS];
+  let mut seen = [false; MAX_PATTERNS];
   for mut num in input.lines().map(|s| s.parse().unwrap()) {
     cur_cost = num % 10;
-    for i in 1..4 {
-      prev_cost = cur_cost;
-      num = calc(num);
-      cur_cost = num % 10;
-      pattern[i] = 9 + cur_cost - prev_cost;
-    }
-    for _ in 3..MAX_ITERATIONS {
+    for t in 0..MAX_ITERATIONS {
       for i in 0..3 {
-        pattern[i] = pattern[i+1];
+        pattern[i] = pattern[i+1]*19;
       }
       prev_cost = cur_cost;
       num = calc(num);
       cur_cost = num % 10;
       pattern[3] = 9 + cur_cost - prev_cost;
-      if !seen[pattern[0]][pattern[1]][pattern[2]][pattern[3]] {
-        seen[pattern[0]][pattern[1]][pattern[2]][pattern[3]] = true;
-        data[pattern[0]][pattern[1]][pattern[2]][pattern[3]] += cur_cost as u16;
+      id = pattern[0] + pattern[1] + pattern[2] + pattern[3];
+      if t > 2 && !seen[id] {
+        seen[id] = true;
+        data[id] += cur_cost as u16;
       }
     }
-
-    for i in 0..19 {
-      for j in 0..19 {
-        for k in 0..19 {
-          for l in 0..19 {
-            seen[i][j][k][l] = false;
-          }
-        }
-      }
-    }
+    seen.fill(false);
+    pattern.fill(0);
   }
 
-  let mut ans = 0;
-  for i in 0..19 {
-    for j in 0..19 {
-      for k in 0..19 {
-        for l in 0..19 {
-          ans = ans.max(data[i][j][k][l]);
-        }
-      }
-    }
-  }
-  ans as usize
+  *data.iter().max().unwrap() as usize
 }
 
 #[cfg(test)]
